421a6da738ee91395b59fdbd4504429791685489
Shi Yong <xtor369@gmail.com>
2026-03-09 01:16:03 +0800

    Implement 'remove' command to remove module #22

--- src/main/java/seedu/duke/module/ModuleList.java
+++ src/main/java/seedu/duke/module/ModuleList.java
@@ -3,7 +3,6 @@ import java.util.List;
 
 public class ModuleList {
     private final List<Module> completedModules;
-
     public ModuleList(List<Module> completedModules) {
         this.completedModules = completedModules;
     }
@@ -12,5 +11,18 @@ public class ModuleList {
         newModule.markCompleted();
         completedModules.add(newModule);
     }
+
+    public boolean removeModule(String moduleCode) {
+        for (int i = 0; i < completedModules.size(); i++){
+            Module module = completedModules.get(i);
+            if (module.getModuleCode().equalsIgnoreCase(moduleCode)) {
+                module.markIncompleted();
+                completedModules.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
